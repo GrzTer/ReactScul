d54d0d0ac50f6bba1d1768bc55656f9fcf3b08d3
GrzegorzT <grzegorz.tereszkiewicz@infotech.edu.pl>
2024-10-24 10:11:20 +0200

    Zad.04

--- reactscul/src/components/Note/note.js
+++ reactscul/src/components/Note/note.js
@@ -3,38 +3,70 @@ import IconPencilSquare from '../Svg/Pen'
 import IconDeleteCircleOutline from '../Svg/Bin'
 import Modal from '../Modal/modal'
 
-const Note = ({ title: initialTitle, description: initialDescription }) => {
-  const [title, setTitle] = useState(initialTitle)
-  const [description, setDescription] = useState(initialDescription)
+const Note = ({ title, description, deleteNote }) => {
   const [isModal, setIsModal] = useState(false)
   const [isExpanded, setIsExpanded] = useState(false)
+  const [currentTitle, setCurrentTitle] = useState(title)
+  const [currentDescription, setCurrentDescription] = useState(description)
   const toggleContent = () => {
     setIsExpanded(!isExpanded)
   }
   return (
     <div className="m-4 transform rounded-lg bg-white p-4 shadow-lg transition hover:scale-105 hover:shadow-xl">
-      <button onClick={toggleContent}>
-        <h2 className="pr-4 mb-2 cursor-pointer text-2xl font-semibold text-gray-800">
-          {title}
-        </h2>
-      </button>
+      <div className="flex items-center justify-between">
+        <button onClick={toggleContent} className="flex-grow text-left">
+          <h2 className="mb-2 text-2xl font-semibold text-gray-800">
+            {currentTitle}
+          </h2>
+        </button>
 
-      <button
-        type="button"
-        className="rounded-full bg-amber-500 px-4 py-2 text-white transition duration-300 hover:bg-blue-600"
-        onClick={() => setIsModal(true)}
-      >
-        <IconPencilSquare />
-      </button>
-      <button
-        type="button"
-        className="rounded-full bg-amber-500 px-4 py-2 text-white transition duration-300 hover:bg-red-600"
-      >
-        <IconDeleteCircleOutline />
-      </button>
+        <div className="flex gap-2">
+          <button
+            type="button"
+            className="justify-end rounded-full bg-amber-500 px-4 py-2 text-white transition duration-300 hover:bg-blue-600"
+            onClick={() => setIsModal(true)}
+          >
+            <IconPencilSquare />
+          </button>
+          <button
+            type="button"
+            className="rounded-full bg-amber-500 px-4 py-2 text-white transition duration-300 hover:bg-red-600"
+            onClick={deleteNote}
+          >
+            <IconDeleteCircleOutline />
+          </button>
+        </div>
+      </div>
 
-      {isExpanded && <span className="mb-4 text-gray-600">{description}</span>}
-      <Modal active={isModal} setIsModal={setIsModal} />
+      {isExpanded && <p className="mb-4 text-gray-600">{currentDescription}</p>}
+      <Modal active={isModal} setIsModal={setIsModal}>
+        <h2 className="ftex-2xl font-bold nb-8">Edite Noet</h2>
+        <form className="flex flex-col gap-2">
+          <label htmlFor="title">Title: </label>
+          <input
+            id="title"
+            name="title"
+            type={'text'}
+            value={currentTitle}
+            onChange={(event) => setCurrentTitle(event.target.value)}
+          />
+          <label htmlFor="description">Description</label>
+          <input
+            id="description"
+            name="description"
+            type={'text'}
+            value={currentDescription}
+            onChange={(event) => setCurrentDescription(event.target.value)}
+          />
+          <button
+            type={'button'}
+            className="py-2 px-4 bg-white rounded hover:text-indigo-700 transition"
+            onClick={() => setIsModal(false)}
+          >
+            Submit
+          </button>
+        </form>
+      </Modal>
     </div>
   )
 }
--- reactscul/src/components/NotesList/notelist.js
+++ reactscul/src/components/NotesList/notelist.js
@@ -1,21 +1,90 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Note from '../Note/note'
 import { NOTES } from './data'
+import Modal from '../Modal/modal'
 
 const NotesList = () => {
+  const [currentNotes, setCurentNotes] = useState(NOTES)
+  const [isModal, setIsModal] = useState(false)
+  const [newTitle, setNewTitle] = useState('')
+  const [newDescription, setNewDescription] = useState('')
+  const [currentDescription, setCurrentDescription] = useState('')
+
+  const deleteNote = (id) => {
+    setCurentNotes(currentNotes.filter((note) => note.id !== id))
+  }
+
+  const addNote = () => {
+    if (newTitle.trim() && newDescription.trim()) {
+      setCurentNotes([
+        ...currentNotes,
+        { id: Math.random(), title: newTitle, description: newDescription },
+      ])
+      setNewTitle('')
+      setNewDescription('')
+      setIsModal(false)
+    }
+  }
+
   return (
     <div className="bg-amber-500 p-10 rounded-xl flex flex-col gap-4 scale-125">
       <h1 className="mb-6 text-center text-5xl font-bold">Notatki</h1>
       <hr />
       <div>
-        {NOTES.map((currentNote) => (
+        {currentNotes.map((currentNote) => (
           <Note
             key={currentNote.id}
             title={currentNote.title}
             description={currentNote.description}
+            deleteNote={() => deleteNote(currentNote.id)}
           />
         ))}
       </div>
+      <hr />
+      <button
+        type="button"
+        className="rounded-full bg-white px-4 py-2 text-amber-500 transition duration-300 hover:bg-black hover:text-white"
+        onClick={() => setIsModal(true)}
+      >
+        Add Note
+      </button>
+      <Modal active={isModal} setIsModal={setIsModal}>
+        <h2 className="text-2xl font-bold mb-8">Add Note</h2>
+        <form
+          className="flex flex-col gap-2"
+          onSubmit={(e) => e.preventDefault()}
+        >
+          <label htmlFor="title">Title: </label>
+          <input
+            id="title"
+            name="title"
+            type="text"
+            value={newTitle} // Bind input to the state
+            onChange={(event) => setNewTitle(event.target.value)} // Update state on change
+            placeholder="Enter note title"
+            className="p-2 rounded"
+          />
+
+          <label htmlFor="description">Description:</label>
+          <input
+            id="description"
+            name="description"
+            type="text"
+            value={newDescription}
+            onChange={(event) => setNewDescription(event.target.value)} // Update state on change
+            placeholder="Enter note description"
+            className="p-2 rounded"
+          />
+
+          <button
+            type="button"
+            className="py-2 px-4 bg-white rounded hover:text-indigo-700 transition mt-4"
+            onClick={addNote}
+          >
+            Submit
+          </button>
+        </form>
+      </Modal>
     </div>
   )
 }
