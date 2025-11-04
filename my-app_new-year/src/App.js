88645f4b745c5edd4026eb55dd6f5a4a96085c44
GrzegorzT <144318154+GrzTer@users.noreply.github.com>
2025-11-04 09:32:27 +0100

    Add product list and reklama1 image to app
    Introduced a productList array in App.js and rendered products dynamically, including their images and prices. Added reklama1.png to the public images folder and updated CSS to add a yellow border to advertisement images.

--- my-app_new-year/src/App.js
+++ my-app_new-year/src/App.js
@@ -1,6 +1,17 @@
 import './App.css';
 
 function App() {
+  const productList = [
+    {id: "0",
+      name: "Nazwa Produktu",
+      price: 1212,
+      currency: "PLN",
+      img: {
+        width: 200,
+        url: "/images/reklama1.png"}
+    },
+
+  ]
   return (
     <>
       <header className='header'>
@@ -9,11 +20,11 @@ function App() {
         </div>
       </header>
       <main className='main'>
-          <img className='img-ads' src="/reklama1.png"/>
+        <img className='img-ads' src="/reklama1.png" />
         <div className='container main-content'>
           <aside className='filter-aside'>
             <h2 className='conteiner-title'>PANEELL BOCZNYY</h2>
-            <hr/>
+            <hr />
             <ol>
               <li>Filtr 1</li>
               <ul>
@@ -35,66 +46,25 @@ function App() {
           </aside>
           <section className='products-list'>
             <h2 className='conteiner-title'>ZAWAARRTTTOOŚĆ</h2>
-            <hr/>
+            <hr />
             <ul className='list-container'>
-              <li className='product-tile'>
-                <h3>Nazwa Produktu</h3>
-                <div className='tile-line'/>
-                <span>99zł</span>
+              {
+                productList.map(
+                (element) => 
+                <li className='product-tile'>
+                <h3>{element.name}</h3>
+                <div className='tile-line' />
+                <img src={element.img.url}/>
+                <span>{element.price} {element.currency}</span>
                 <button type='button' className='tile-button'>Dodaj Do Koszyka</button>
               </li>
-              <li className='product-tile'>
-                <h3>Nazwa Produktu</h3>
-                <div className='tile-line'/>
-                <span>99zł</span>
-                <button type='button' className='tile-button'>Dodaj Do Koszyka</button>
-              </li>
-              <li className='product-tile'>
-                <h3>Nazwa Produktu</h3>
-                <div className='tile-line'/>
-                <span>99zł</span>
-                <button type='button' className='tile-button'>Dodaj Do Koszyka</button>
-              </li>
-              <li className='product-tile'>
-                <h3>Nazwa Produktu</h3>
-                <div className='tile-line'/>
-                <span>99zł</span>
-                <button type='button' className='tile-button'>Dodaj Do Koszyka</button>
-              </li>
-              <li className='product-tile'>
-                <h3>Nazwa Produktu</h3>
-                <div className='tile-line'/>
-                <span>99zł</span>
-                <button type='button' className='tile-button'>Dodaj Do Koszyka</button>
-              </li>
-              <li className='product-tile'>
-                <h3>Nazwa Produktu</h3>
-                <div className='tile-line'/>
-                <span>99zł</span>
-                <button type='button' className='tile-button'>Dodaj Do Koszyka</button>
-              </li>
-              <li className='product-tile'>
-                <h3>Nazwa Produktu</h3>
-                <div className='tile-line'/>
-                <span>99zł</span>
-                <button type='button' className='tile-button'>Dodaj Do Koszyka</button>
-              </li>
-              <li className='product-tile'>
-                <h3>Nazwa Produktu</h3>
-                <div className='tile-line'/>
-                <span>99zł</span>
-                <button type='button' className='tile-button'>Dodaj Do Koszyka</button>
-              </li>
-              <li className='product-tile'>
-                <h3>Nazwa Produktu</h3>
-                <div className='tile-line'/>
-                <span>99zł</span>
-                <button type='button' className='tile-button'>Dodaj Do Koszyka</button>
-              </li>
+              )
+              }
+
             </ul>
           </section>
         </div>
-          <img className='img-ads' src="reklama2.png"/>
+        <img className='img-ads' src="reklama2.png" />
       </main>
       <footer className='footer'>
         <div className='container'>
